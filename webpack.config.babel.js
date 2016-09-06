721ac9a437a63b5118f6a882029dee946025dd99
Fernando Flores V <ferflores@users.noreply.github.com>
2016-09-06 11:55:23 -0500

    bower components are not being used

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -12,7 +12,7 @@ module.exports = {
     loaders: [
       {
         test: /\.js/,
-        exclude: /(node_modules|bower_components)/,
+        exclude: /(node_modules)/,
         loader: 'babel',
         query: {
           presets: ['es2015']
